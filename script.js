d9ae62260f6441c785f5894682c75173add495f3
vishal <vk43840@gmail.com>
2024-07-02 10:49:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function AboutEngmates() {
+   var paragraph = document.getElementById("Showabout");
+   if (paragraph.style.display === "none") {
+     paragraph.style.display = "block";
+   } else {
+     paragraph.style.display = "none";
+   }
+ }
